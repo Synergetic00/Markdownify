597f23dcc7ef67f55a4f3fe193ad9df90fe46665
Synergetic00 <synergetic00@gmail.com>
2021-09-20 16:30:15 +1000

    excel file added

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 import zipfile
 
-with zipfile.ZipFile("test01.docx","r") as docx:
+with zipfile.ZipFile("word01.docx","r") as docx:
     file = str(docx.read('word/document.xml').decode('ascii'))
 
 print(file)
