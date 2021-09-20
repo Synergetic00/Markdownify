aff5b7fb9ab4c8eaef5ddaf5007b71299ae571c3
Synergetic00 <synergetic00@gmail.com>
2021-09-20 17:33:48 +1000

    started on excel parsing

--- reader.py
+++ reader.py
@@ -1,6 +1,13 @@
 import zipfile
 
-with zipfile.ZipFile("word01.docx","r") as docx:
-    file = str(docx.read('word/document.xml').decode('ascii'))
+def getWordContent(path):
+    with zipfile.ZipFile(path, "r") as docx:
+        return str(docx.read('word/document.xml').decode('ascii'))
 
-print(file)
+def getExcelContent(path):
+    with zipfile.ZipFile(path, "r") as docx:
+        strings = str(docx.read('xl/sharedStrings.xml').decode('ascii'))
+        return [strings]
+
+#print(getWordContent('word01.docx'))
+print(getExcelContent('excel01.xlsx'))
