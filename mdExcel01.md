a47a4f9cdbd6208a9a42621f401ead5ee2c22f30
Synergetic00 <synergetic00@gmail.com>
2021-09-30 20:33:55 +1000

    Added basic table converter for Excel

---
+++ mdExcel01.md
@@ -0,0 +1,6 @@
+|Duplicate|Heading|Duplicate|
+|:-:|:-:|:-:|
+|C1R1|C2R1|C3R1|
+|C1R2|C2R2|C3R2|
+|C1R3|C3R3|C3R3|
+|C1R4|C2R3|C3R4|
--- reader.py
+++ reader.py
@@ -6,22 +6,25 @@ def getWordContent(path):
     with zipfile.ZipFile(path, "r") as docx:
         return str(docx.read('word/document.xml').decode('ascii'))
 
-def lettersToValue(input):
+def getValueOfLetters(input):
     counter = 0
-    pos = len(input)
-    for letter in input:
+    pos = 0
+    for letter in input[::-1]:
         factor = 26 ** pos
-        print(pos)
-        counter += factor * string.ascii_uppercase.index(letter)
-        pos -= 1
-    return counter
+        counter += factor * (string.ascii_uppercase.index(letter) + 1)
+        pos += 1
+    return counter - 1
+
+def getExcelCellPosition(input):
+    details = re.split('(\d+)', input)
+    return [int(details[1])-1, getValueOfLetters(details[0])]
 
 def getExcelSheetSize(dimensions):
-    start = re.split('(\d+)', dimensions[0])[0:2]
-    end = re.split('(\d+)', dimensions[1])[0:2]
-    startPos = [lettersToValue(start[0]), int(start[1])]
-    endPos = [lettersToValue(end[0]), int(end[1])]
-    return [startPos, endPos]
+    start = getExcelCellPosition(dimensions[0])
+    end = getExcelCellPosition(dimensions[1])
+    width = (end[0]-start[0]) + 1
+    height = (end[1]-start[1]) + 1
+    return [width, height]
 
 def getExcelContent(path):
     with zipfile.ZipFile(path, "r") as docx:
@@ -33,11 +36,24 @@ def getExcelContent(path):
         dimPat = re.compile('dimension ref="(.+)"/><sheetViews')
         dimensions = dimPat.findall(sheet1)[0].split(':')
         sheetSize = getExcelSheetSize(dimensions)
-        return [strings, sheet1]
+        cellPat = re.compile('<c r="([A-Z]\w+)" t="s"><v>([0-9]+)<\/v><\/c>')
+        cells = cellPat.findall(sheet1)
+        array = [['' for x in range(sheetSize[1])] for y in range(sheetSize[0])]
+        for cell in cells:
+            pos = getExcelCellPosition(cell[0])
+            array[int(pos[0])][int(pos[1])] = strings[int(cell[1])]
+        return array
+
+def makeBasicMarkdownTable(array, filename):
+    output = ''
+    for i in range(len(array)):
+        output += '|' + '|'.join(array[i]) + '|\n'
+        if i == 0:
+            output += '|' + (':-:|' * len(array[0])) + '\n'
+    with open(filename+'.md', 'w') as f:
+        f.write(output.strip())
+
 
 #print(getWordContent('word01.docx'))
 
-#excelContent = getExcelContent('excel01.xlsx')
-#print(excelContent)
-
-print(getExcelSheetSize(['A1', 'AC5']))
+makeBasicMarkdownTable(getExcelContent('excel01.xlsx'), 'mdExcel01')
